15342ea3af2d2a93fec4d174cf98086642eb3d9f
Frank Rohlfing <mail@frank-rohlfing.de>
2017-02-09 17:57:36 +0100

    add packages.json

--- public/index.php
+++ public/index.php
@@ -21,32 +21,3 @@ require __DIR__ . '/../vendor/autoload.php';
  * Run...
  */
 \Core\Application::run();
-
-
-///*
-// * Push the Services into the Dependency Injector.
-// */
-//call_user_func(function() {
-//    require __DIR__ . '/../boot/services.php';
-//});
-//
-///*
-// * Bootstraps the framework
-// */
-//(new Bootstraps\LoadConfiguration)->bootstrap();
-//(new Bootstraps\HandleExceptions)->bootstrap();
-//(new Bootstraps\HandleShutdown)->bootstrap();
-//
-///*
-// * Register routes.
-// */
-//call_user_func(function() {
-//    require __DIR__ . '/../boot/routes.php';
-//});
-//
-///*
-// * Dispatch the HTTP request and send the response to the browser.
-// */
-//App\Services\DI::getInstance()->get('route')->dispatch(
-//    App\Services\DI::getInstance()->get('request')
-//)->send();
