b111724558d7ceef081c69beab411fbdfefd7895
Frank Rohlfing <mail@frank-rohlfing.de>
2017-02-23 21:09:03 +0100

    add mink

---
+++ tests/ExampleTest.php
@@ -0,0 +1,31 @@
+<?php
+
+use Core\Testing\MinkTestCase;
+
+class ExampleTest extends MinkTestCase
+{
+    /**
+     * A basic functional test example.
+     */
+    public function testBasicExample()
+    {
+        $this->assertEquals(1, 1);
+    }
+
+    /**
+     * A mink test example.
+     */
+    public function testMinkExample()
+    {
+        $url = config('app.url');
+        $this->session->visit($url);
+        $page = $this->session->getPage();
+
+        $title = $page->find('xpath', '//title');
+        if ($title !== null) {
+            $title = $title->getText();
+        }
+
+        $this->assertEquals($title, 'Pletfix Workbench');
+    }
+}
