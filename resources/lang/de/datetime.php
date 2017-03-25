9fe632147ab11dd9e2fdf680ba03a5524b3447c2
Frank Rohlfing <mail@frank-rohlfing.de>
2017-03-25 02:44:39 +0100

    update DateTime

---
+++ resources/lang/de/datetime.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Time and date formats
+ *
+ * Apply the format options from PHP's [date_create_from_format](http://php.net/manual/en/datetime.createfromformat.php)
+ * function.
+ */
+
+return [
+
+    'datetime' => 'd.m.Y H:i',
+    'date'     => 'd.m.Y',
+    'time'     => 'H:i',
+
+];
---
+++ resources/lang/en/datetime.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Time and date formats.
+ *
+ * Apply the format options from PHP's [date_create_from_format](http://php.net/manual/en/datetime.createfromformat.php)
+ * function.
+ */
+
+return [
+
+    'datetime' => 'Y-m-d H:i',
+    'date'     => 'Y-m-d',
+    'time'     => 'H:i',
+
+];
