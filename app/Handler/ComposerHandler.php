adf250db14bbe1d6785bf6b2248a259f3b64f886
Frank Rohlfing <mail@frank-rohlfing.de>
2017-09-07 21:06:25 +0200

    add colorable output for ComposerHandler

--- app/Handler/ComposerHandler.php
+++ app/Handler/ComposerHandler.php
@@ -8,7 +8,7 @@ use Exception;
 /**
  * Class ComposerHandler
  *
- * This class is entered in the `composer.json` file.
+ * This class is used for the installation procedure.
  *
  * You can execute the scripts manually. E.g. enter this into your terminal to run the `post-root-package-install` event:
  *
@@ -25,6 +25,186 @@ use Exception;
  */
 class ComposerHandler
 {
+    /*
+     * Styles.
+     *
+     * Based on the `ANSI/VT100 Terminal Control reference` at <http://www.termsys.demon.co.uk/vtansi.htm>.
+     * @see https://github.com/auraphp/Aura.Cli/blob/2.x/src/Stdio/Formatter.php
+     * @see https://www.if-not-true-then-false.com/2010/php-class-for-coloring-php-command-line-cli-scripts-output-php-output-colorizing-using-bash-shell-colors/
+     * @see https://github.com/cakephp/cakephp/blob/3.next/src/Console/ConsoleOutput.php
+     */
+    //const STYLE_RESET    = 0;
+    const STYLE_BOLD       = 1;
+    const STYLE_DIM        = 2;
+    const STYLE_UL         = 4;
+    const STYLE_BLINK      = 5;
+    const STYLE_REVERSE    = 7;
+    const STYLE_BLACK      = 30;
+    const STYLE_RED        = 31;
+    const STYLE_GREEN      = 32;
+    const STYLE_YELLOW     = 33;
+    const STYLE_BLUE       = 34;
+    const STYLE_MAGENTA    = 35;
+    const STYLE_CYAN       = 36;
+    const STYLE_WHITE      = 37;
+    const STYLE_BLACK_BG   = 40;
+    const STYLE_RED_BG     = 41;
+    const STYLE_GREEN_BG   = 42;
+    const STYLE_YELLOW_BG  = 43;
+    const STYLE_BLUE_BG    = 44;
+    const STYLE_MAGENTA_BG = 45;
+    const STYLE_CYAN_BG    = 46;
+    const STYLE_WHITE_BG   = 47;
+
+    /** @noinspection PhpUndefinedClassInspection */
+    /**
+     * @var Event
+     */
+    private $event;
+
+    public function __construct (/** @noinspection PhpUndefinedClassInspection */ Event $event)
+    {
+        $this->event = $event;
+    }
+
+    /**
+     * Format text.
+     *
+     * @param string $text The message
+     * @param array $styles Combination of Stdio::STYLE constants
+     * @return string
+     */
+    private function format($text, array $styles = [])
+    {
+        /** @noinspection PhpUndefinedMethodInspection */
+        if (!$this->event->getIO()->isDecorated()) {
+            return $text;
+        }
+
+        return "\033[" . implode(';', $styles) . "m" . $text . "\033[0m";
+    }
+
+    /**
+     * Writes a message in white to the output.
+     *
+     * @param string $message
+     */
+    protected function write($message)
+    {
+        /** @noinspection PhpUndefinedMethodInspection */
+        $this->event->getIO()->write($message);
+    }
+
+    /**
+     * Writes a message in yellow to the output.
+     *
+     * @param string $message
+     */
+    protected function writeHint($message)
+    {
+        /** @noinspection PhpUndefinedMethodInspection */
+        $this->event->getIO()->write($this->format($message, [self::STYLE_YELLOW]));
+    }
+
+    /**
+     * Writes a message in green to the output.
+     *
+     * @param string $message
+     */
+    protected function writeInfo($message)
+    {
+        /** @noinspection PhpUndefinedMethodInspection */
+        $this->event->getIO()->write($this->format($message, [self::STYLE_GREEN]));
+    }
+
+    /**
+     * Asks a question to the user.
+     *
+     * @param string $question The question to ask.
+     * @param string $default The default answer if none is given by the user.
+     * @return string
+     */
+    protected function ask($question, $default = null)
+    {
+        $prompt = $this->format($question, [self::STYLE_GREEN]);
+        if ($default !== null) {
+            $prompt .= ' [' . $this->format($default, [self::STYLE_YELLOW]) . ']';
+        }
+        $prompt .= ':' . PHP_EOL . '> ';
+
+        /** @noinspection PhpUndefinedMethodInspection */
+        return $this->event->getIO()->ask($prompt, $default);
+    }
+
+    /**
+     * Asks for a value and validates the response.
+     *
+     * The validator receives the data to validate. It must return the validated data when the data is valid and throw
+     * an exception otherwise.
+     *
+     * @param string $question The question to ask.
+     * @param callable $validator A PHP callback.
+     * @param mixed $default The default answer if none is given by the user.
+     * @return mixed
+     */
+    protected function askAndValidate($question, callable $validator, $default = null)
+    {
+        $prompt = $this->format($question, [self::STYLE_GREEN]);
+        if ($default !== null) {
+            $prompt .= ' [' . $this->format($default, [self::STYLE_YELLOW]) . ']';
+        }
+        $prompt .= ':' . PHP_EOL . '> ';
+
+        /** @noinspection PhpUndefinedMethodInspection */
+        return $this->event->getIO()->askAndValidate($prompt, $validator, null, $default);
+    }
+
+    /**
+     * Asks a confirmation to the user.
+     *
+     * The question will be asked until the user answers by nothing, yes, or no.
+     *
+     * @param string $question The question to ask.
+     * @param bool $default The default answer if none is given by the user.
+     * @return bool
+     */
+    protected function askConfirmation($question, $default = true)
+    {
+        $prompt = $this->format($question . ' (yes/no)', [self::STYLE_GREEN])
+            . ' [' . $this->format($default, [self::STYLE_YELLOW]) . ']'
+            . ':' . PHP_EOL . '> ';
+
+        /** @noinspection PhpUndefinedMethodInspection */
+        return $this->event->getIO()->askConfirmation($prompt, $default);
+    }
+
+    /**
+     * Asks the user to select a value.
+     *
+     * @param string $question The question to ask.
+     * @param string|null $default The default answer if none is given by the user.
+     * @return string
+     */
+    protected function select($question, array $choices, $default = null)
+    {
+        $prompt = $this->format($question, [self::STYLE_GREEN]);
+        if ($default !== null) {
+            $prompt .= ' [' . $this->format($default, [self::STYLE_YELLOW]) . ']';
+        }
+
+        /** @noinspection PhpUndefinedMethodInspection */
+        return $this->event->getIO()->select($prompt, $choices, $default);
+    }
+
+    /**
+     * Include the composer autoload file.
+     */
+    protected function autoload()
+    {
+        /** @noinspection PhpUndefinedMethodInspection, PhpIncludeInspection */
+        require_once $this->event->getComposer()->getConfig()->get('vendor-dir') . '/autoload.php';
+    }
+
     /** @noinspection PhpUndefinedClassInspection */
     /**
      * Handle the post-root-package-install Composer event.
@@ -35,9 +215,10 @@ class ComposerHandler
      */
     public static function postRootPackageInstall(/** @noinspection PhpUndefinedClassInspection */ Event $event)
     {
-        self::createEnvironmentFile($event);
-        self::createStorageFolder($event);
-        self::createDatabase($event);
+        $handler = new static($event);
+        $handler->createEnvironmentFile();
+        $handler->createStorageFolder();
+        $handler->createDatabase();
     }
 
     /** @noinspection PhpUndefinedClassInspection */
@@ -50,64 +231,56 @@ class ComposerHandler
      */
     public static function postCreateProjectCmd(/** @noinspection PhpUndefinedClassInspection */ Event $event)
     {
-        self::migrateDatabase($event);
+        $handler = new static($event);
+        $handler->migrateDatabase();
     }
 
-    /** @noinspection PhpUndefinedClassInspection */
     /**
      * Create the database
-     *
-     * @param Event $event
      */
-    private static function createEnvironmentFile(/** @noinspection PhpUndefinedClassInspection */ Event $event)
+    private function createEnvironmentFile()
     {
         if (file_exists('.env')) {
             return;
         }
 
-        /** @noinspection PhpUndefinedMethodInspection */
-        $io = $event->getIO();
-
-        /** @noinspection PhpUndefinedMethodInspection */
-        $io->write('Create environment file...');
-
         if (copy('.env.example', '.env')) {
             /** @noinspection PhpUndefinedMethodInspection */
-            $io->write('Environment file successfully created.');
+            $this->write('Environment file created.');
         }
     }
 
-    /** @noinspection PhpUndefinedClassInspection */
     /**
      * Create the storage folder
-     *
-     * @param Event $event
      */
-    private static function createStorageFolder(/** @noinspection PhpUndefinedClassInspection */ Event $event)
+    private function createStorageFolder()
     {
-        /** @noinspection PhpUndefinedMethodInspection */
-        $io = $event->getIO();
+        $this->write('Create storage folder...');
 
-        /** @noinspection PhpUndefinedMethodInspection */
-        $io->write('Create storage folder...');
+        $this->writeHint('Enter the file mode and group for the directories that are created in the storage folder.');
+        $this->writeHint('Note, that the directories within the storage folder must be writable by your web server!');
+        $this->writeHint('Enter "-" to skip this part. In this case you have to set the permission after the installation procedure manually.');
 
         if (!file_exists('storage')) {
             mkdir('storage');
         }
 
-        /** @noinspection PhpUndefinedMethodInspection */
-        $mode = $io->askAndValidate('File mode for files created in the storage folder? Enter "-" to skip. [2775]', function ($mode) {
+        $mode = $this->askAndValidate('File mode?', function ($mode) {
             if ($mode == '-') {
                 return $mode;
             }
             if (!preg_match('/^([0-7])?[0-7][0-7][0-7]$/', $mode, $matches)) {
                 throw new Exception(sprintf('Mode "%s" is invalid. The mode consists of three or four octal digits (0..7).', $mode));
             }
-            return intval($mode, 8);
-        }, null, 02775);
+            return $mode;
+        }, '2775');
+
+        if ($mode != '-') {
+            $mode = intval($mode, 8);
+        }
 
         /** @noinspection PhpUndefinedMethodInspection */
-        $group = $io->ask('File group for files created in the storage folder? Enter "-" to skip. [www-data]', 'www-data');
+        $group = $this->ask('File group?', 'www-data');
 
         foreach (['cache', 'logs', 'db'] as $folder) {
             $path = 'storage/' . $folder;
@@ -123,63 +296,36 @@ class ComposerHandler
         }
 
         /** @noinspection PhpUndefinedMethodInspection */
-        $io->write('Storage folder successfully created.');
+        $this->write('Storage folder successfully created.');
     }
 
-    /** @noinspection PhpUndefinedClassInspection */
     /**
      * Create the database
-     *
-     * @param Event $event
      */
-    private static function createDatabase(/** @noinspection PhpUndefinedClassInspection */ Event $event)
+    private function createDatabase()
     {
         if (file_exists('storage/db/sqlite.db')) {
             return;
         }
 
-        /** @noinspection PhpUndefinedMethodInspection */
-        $io = $event->getIO();
-
-        /** @noinspection PhpUndefinedMethodInspection */
-        if ($io->askConfirmation('Create a SQLite database (y/n)? [y]')) {
-            touch('storage/db/sqlite.db');
+        if ($this->askConfirmation('Create a SQLite database?')) {
+            if (touch('storage/db/sqlite.db')) {
+                $this->write('Database successfully created.');
+            }
         }
     }
 
-
-    /** @noinspection PhpUndefinedClassInspection */
     /**
      * Create the database
-     *
-     * @param Event $event
      */
-    private static function migrateDatabase(/** @noinspection PhpUndefinedClassInspection */ Event $event)
+    private function migrateDatabase()
     {
         if (!file_exists('storage/db/sqlite.db')) {
             return;
         }
 
-        /** @noinspection PhpUndefinedMethodInspection */
-        $io = $event->getIO();
-
-        /** @noinspection PhpUndefinedMethodInspection */
-        $io->write('Migrate the database...');
+        $this->write('Migrate the database...');
 
         system('php console migrate');
     }
-
-//    /** @noinspection PhpUndefinedClassInspection */
-//    /**
-//     * Handle the post-create-project-cmd Composer event.
-//     *
-//     * Occurs after the root package has been installed, during the create-project command.
-//     *
-//     * @param Event $event
-//     */
-//    private static function autoload(/** @noinspection PhpUndefinedClassInspection */ Event $event)
-//    {
-//        /** @noinspection PhpUndefinedMethodInspection, PhpIncludeInspection */
-//        require_once $event->getComposer()->getConfig()->get('vendor-dir') . '/autoload.php';
-//    }
 }
